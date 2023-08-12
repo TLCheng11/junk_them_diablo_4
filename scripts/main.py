09e3d98fd5aa564016108d42777fe877c973044d
Scorpio <scorpio_ctl@hotmail.com>
2023-08-12 15:28:00 -0400

    clean up main.py test data

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,6 @@
-import pyautogui
-from items_scaner import start_scan
 from ui_main_window import open_ui
 
 if __name__ == "__main__":
-    window_size = pyautogui.size()
     open_ui()
-    # start_scan(window_size, inventory_slot_to_check)
     
     
