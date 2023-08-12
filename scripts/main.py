dd14c3baa1957c41256189aaf83c24a878fc6913
Scorpio <scorpio_ctl@hotmail.com>
2023-08-12 15:27:06 -0400

    clean up main.py test data

--- scripts/main.py
+++ scripts/main.py
@@ -2,18 +2,6 @@ import pyautogui
 from items_scaner import start_scan
 from ui_main_window import open_ui
 
-# inventory_slot_to_check = [
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]
-# ]
-# for testing
-# inventory_slot_to_check = [
-#     [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# ]
-
 if __name__ == "__main__":
     window_size = pyautogui.size()
     open_ui()
